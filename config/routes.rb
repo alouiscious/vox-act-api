8665b1103e5d57b8503fdd32143298cfc306a7f3
Alouiscious Media <info@reneesoul.com>
2020-09-26 18:37:09 -0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#delete'
+  get 'get_current_user', to: 'sessions#get_current_user'
   resources :secrets
+  resources :talents
+  resources :agents
+  resources :clients
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
